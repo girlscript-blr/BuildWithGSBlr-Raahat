390d518013ce00a8850627dbf4ca51808a057d8e
Girlscript Bangalore <54791364+girlscript-blr@users.noreply.github.com>
2020-10-03 00:25:19 +0530

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,7 +3,6 @@ import './header.css'
 
 const Header = () => {
     return (
-        // Reference of how to use bulma classes
         <div className="section has-text-centered is-size-1">
             Raahat App
         </div>
