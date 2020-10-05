6b4b739a779bb400f70f78a19a7b36d9ef8a2eab
Purnima Sharma <57852378+purnima143@users.noreply.github.com>
2020-10-05 23:30:41 +0530

    created hero component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
-import Header from './components/header/header'
+import Header from './components/header/header';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div>
+    <Header/>
       <Router>
       <Switch>
-      <Route path="/" exact component={Header}/>
+      <Route path="/" exact component={Home}/>
       </Switch>
       </Router>
  
