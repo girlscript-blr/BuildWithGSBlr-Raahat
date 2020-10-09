d35d9628dc6229b3123cf05ed7869e91239e1fac
Harshitha-sa <saharshithasa@gmail.com>
2020-10-09 22:28:05 +0530

    create surveyresult for home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 import Header from './components/header/header';
 import Home from './components/home/Home';
+import surveyResult from './components/home/surveyResult';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Router>
       <Switch>
       <Route path="/" exact component={Home}/>
+      <Route path="/surveyResult" exact component={surveyResult}/>
       </Switch>
       </Router>
  
