e7a47d3881aa87aa46a8767317c30839afb7d804
Prathibha R <prathi2kgrs@gmail.com>
2020-10-09 14:54:05 +0530

    Added Take Survey

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
-import Header from './components/header/header'
+import Header from './components/header/header';
+import TakeSurvey from './components/home/TakeSurvey';
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Router>
       <Switch>
       <Route path="/" exact component={Header}/>
+      <Route path="/takesurvey" exact component={TakeSurvey}/>
       </Switch>
       </Router>
  
