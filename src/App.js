d9833b7bee608111c0cfd3cb28153d2148084b7d
Karshil Sheth <karshil.sheth@gatewaygroup.com>
2020-10-09 16:15:38 +0530

    Refactor the footer code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
-import Header from './components/header/header'
+import footer from './components/Footer/footer';
 
 function App() {
   return (
     <div>
       <Router>
       <Switch>
-      <Route path="/" exact component={Header}/>
+      <Route path="/" exact component={footer}/>
       </Switch>
       </Router>
  
