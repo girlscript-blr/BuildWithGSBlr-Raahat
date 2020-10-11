13a53a61edfac069bdcce768208e3e05f30b54d4
ShivangiRai1310 <shivangirai1310@gmail.com>
2020-10-12 02:41:45 +0530

    Button Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import './App.css';
-import Header from './components/header/header'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div>
       <Router>
-      <Switch>
-      <Route path="/" exact component={Header}/>
-      </Switch>
+        <Switch>
+          <Route path="/" exact component={Header} />
+        </Switch>
       </Router>
     </div>
   );
--- src/components/common/button/button.js
+++ src/components/common/button/button.js
@@ -1,46 +1,35 @@
-import React from 'react'
-import './button.css'
+import React from "react";
+import "./button.css";
 
 const Button = (props) => {
+  console.log(props);
+  let sec;
+  if (props.variant === "secondary") sec = true;
+  else sec = false;
 
-    console.log(props);
-    let sec;
-    if( props.variant === "secondary")
-        sec = true;
-    else
-        sec = false;
-
-    return (
-        <div>  
-            <button 
-                class={ "button " + ( sec ? 'secondary ' : 'primary ') + ( props.isLarge ? 'larger ' : 'smaller ') }
-                onClick={props.onClick}
-                type={props.type}
-                disabled={props.disabled}>
-            {props.label}
-            </button>
-        </div>
-    )
-}
-
-Button.defaultProps = {
-    type: "button",
-    variant: "secondary",
-    isLarge: false,
-    disabled: false,
+  return (
+    <div>
+      <button
+        class={
+          "button " +
+          (sec ? "secondary " : "primary ") +
+          (props.isLarge ? "larger " : "smaller ")
+        }
+        onClick={props.onClick}
+        type={props.type}
+        disabled={props.disabled}
+      >
+        {props.label}
+      </button>
+    </div>
+  );
 };
 
-export default Button
+Button.defaultProps = {
+  type: "button",
+  variant: "secondary",
+  isLarge: false,
+  disabled: false,
+};
 
-/*
-import Button from './components/common/button/button';
-
-
-<Button label="Click Me" />
-<Button label="Text to add" 
-        type="button/submit/reset" 
-        variant="primary/secondary" 
-        isLarge={true/false} 
-        disabled={true/false} 
-        />
-*/
+export default Button;
