841c7e4c8a9794b1d93d2d2ab48878291e734432
Anjali <nova.anjali@gmail.com>
2020-10-12 20:08:59 +0530

    Navbar Update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 import Header from './components/header/header';
 import Home from './components/home/Home';
+import Navbar from './components/common/navbar/navbar'
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Route path="/" exact component={Home}/>
       </Switch>
       </Router>
- 
+
     </div>
   );
 }
--- src/components/common/navbar/navbar.js
+++ src/components/common/navbar/navbar.js
@@ -2,11 +2,12 @@ import React from 'react'
 import './navbar.css'
 
 const Navbar = () => {
-    return (
-        <div>
-            It is a navbar component.
-        </div>
-    )
+  return(
+    <div>
+  
+  </div>
+)
+
 }
 
 export default Navbar
