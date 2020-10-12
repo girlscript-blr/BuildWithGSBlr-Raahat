56d9d19d5aaad119148d89a94a9cdd3354ea93ed
Prathibha R <prathi2kgrs@gmail.com>
2020-10-12 22:27:18 +0530

    Revert "Added Take Survey"
    This reverts commit e7a47d3881aa87aa46a8767317c30839afb7d804.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
-import Header from './components/header/header';
+import Header from './components/header/header'
 import Home from './components/home/Home';
 
 
 function App() {
   return (
     <div>
-    <Header/>
+      <Header/>
       <Router>
       <Switch>
       <Route path="/" exact component={Home}/>
--- src/components/home/TakeSurvey.js
+++ src/components/home/TakeSurvey.js
@@ -4,29 +4,26 @@ import "./TakeSurvey.css";
 const TakeSurvey=()=>{
     return(
     <div>
-    <section className="takesurvey">
-    <div class="takesurvey-body">
-        <div class="container">
-        <div class="columns is-desktop">
-    <div class="column">
-
-        <h1 class="title"> Take Survey?</h1>
-           
-            <div class="subtitle">
-            Please fill in the survey form to avail help from us. This will help us to keep track of the food items and other necessities required by you.
+        <section className="takesurvey">
+            <div class="takesurvey-body">
+                <div class="container">
+                    <div class="columns is-desktop">
+                        <div class="column">
+                            <h1 class="title"> Take Survey?</h1>
+                            
+                                <div class="subtitle">
+                                Please fill in the survey form to avail help from us. This will help us to keep track of the food items and other necessities required by you.
+                                </div>
+                                <div className="takesurveybutton">
+                                    <Button className="takesurveybutton">Take Survey</Button>
+                                </div>
+                        </div>
+                        <div class="column col2"></div>
+                        <div class="column col3"></div>
+                    </div>                
+                </div>
             </div>
-            <div className="takesurveybutton">
-                <Button className="takesurveybutton">Take Survey</Button>
-            </div>
-    </div>
-    <div class="column col2"></div>
-    <div class="column col3"></div>
-    </div>
-
-        
-        </div>
-    </div>
-    </section>
+        </section>
     </div>
     )
 }
