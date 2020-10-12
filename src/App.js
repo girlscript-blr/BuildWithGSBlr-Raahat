72c93962ea36c489493412a24e91cf8495942a1e
Anjali <nova.anjali@gmail.com>
2020-10-12 17:54:57 +0530

    Try at nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
+import Navbar from './components/common/navbar/navbar'
 import Header from './components/header/header'
 
 function App() {
@@ -8,10 +9,12 @@ function App() {
     <div>
       <Router>
       <Switch>
+      <Navbar>
       <Route path="/" exact component={Header}/>
+      </Navbar>
       </Switch>
       </Router>
- 
+
     </div>
   );
 }
--- src/components/common/navbar/navbar.js
+++ src/components/common/navbar/navbar.js
@@ -1,12 +1,66 @@
-import React from 'react'
-import './navbar.css'
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = () => {
+
+    const [active,
+        setActive] = useState(false);
+
+    const handleClick = () => {
+        setActive(!active)
+    }
+
     return (
-        <div>
-            It is a navbar component.
-        </div>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+
+                    <img
+                        src=
+
+                        alt="Logo"
+                        width="130"
+                        height="35"/>
+
+
+                <Link
+                    onClick={handleClick}
+                    active={active}
+                    role="button"
+                    className={active
+                    ? "navbar-burger burger is-active"
+                    : "navbar-burger burger"}
+                    aria-label="menu"
+                    aria-expanded="false"
+                    data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </Link>
+            </div>
+
+            <div
+                onClick={handleClick}
+                active={active}
+                className={active
+                ? "navbar-menu is-active"
+                : "navbar-menu"}>
+                <div class="navbar-center">
+                    <Link exact to="/"
+                        class="navbar-item button is-normal is-light">Home</Link>
+                    <Link exact to="/about"
+                        class="navbar-item button is-normal is-light">About</Link>
+
+                    <Link exact to="/projects"
+                        class="navbar-item button is-normal is-light">Projects</Link>
+                    <Link exact to="/contact"
+                        class="navbar-item button is-normal is-light">Contact</Link>
+                </div>
+            </div>
+        </nav>
+
     )
+
 }
 
-export default Navbar
+export default Navbar;
