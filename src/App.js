7291bbcb9cdaebe1e46036bf135a14a57bc47bad
Purnima Sharma <57852378+purnima143@users.noreply.github.com>
2020-10-19 20:52:50 +0530

    Created Piechart

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
 import Header from './components/header/header';
 import Home from './components/home/Home';
-
+import PieChart from './components/common/piechart/Piechart';
 function App() {
   return (
     <div>
     <Header/>
+    <PieChart/>
       <Router>
       <Switch>
       <Route path="/" exact component={Home}/>
