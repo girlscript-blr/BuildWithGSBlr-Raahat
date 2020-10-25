c6a9375e3851500b8d990c0ec68fbd7762dec628
Kaushik Mishra <kmishra27.km@gmail.com>
2020-10-26 00:48:50 +0530

    Horizontal Bargraph component made

---
+++ src/components/common/barchart/Barchart.js
@@ -0,0 +1,56 @@
+// react
+import React from 'react'
+import PropTypes from 'prop-types'
+
+//component
+import { HorizontalBar } from 'react-chartjs-2';
+
+function Barchart(props) {
+
+    const chartData = {
+        labels : props.labels,
+        datasets : [
+            {   
+                label: props.dataLabel,
+                backgroundColor : props.colour,
+                data: props.record
+            }
+        ],
+    }
+
+    return (
+        <div className="bar">
+            <HorizontalBar
+                data={chartData}
+                options={{
+                    title: {
+                        display: true,
+                        text: props.title,
+                        fontSize: 18,
+                        fontColor: 'black',
+                        fontStyle: 'bold',
+                        fontFamily: 'Arial'
+                    },
+                    legend: {
+                        display: true,
+                        position: 'bottom',
+                        labels : {
+                            fontColor: 'black'
+                        }
+                    }
+                }}
+            />
+        </div>
+    )
+}
+
+Barchart.propTypes = {
+    labels: PropTypes.arrayOf(PropTypes.string),
+    record : PropTypes.arrayOf(PropTypes.number),
+    backgroundColour : PropTypes.string,
+    title : PropTypes.string,
+    dataLabel : PropTypes.string,
+    colour : PropTypes.string,    
+}
+
+export default Barchart;
