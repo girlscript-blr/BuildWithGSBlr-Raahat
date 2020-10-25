319a058ba324d3c4cfb7a0342b22a498e226e569
Kaushik Mishra <kmishra27.km@gmail.com>
2020-10-25 23:23:28 +0530

    react-chartjs-2 installed

---
+++ src/components/common/barchart/barchart.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function barchart(props) {
+
+    // This component should take the following props
+        // labels: list of strings
+        // record: list of integer values to display as per labels
+        // backgroundColour: single color to be passed as per choice
+        // title: The title of the chart
+        // colour: The color of the chart
+    // Create the component as shown in the design.
+    // The component should be responsive so that it looks good in both desktop and mobile view.
+    // The title props value should be the title of the chart.
+
+    return (
+        <div className="chart">
+
+        </div>
+    )
+}
+
+export default barchart
