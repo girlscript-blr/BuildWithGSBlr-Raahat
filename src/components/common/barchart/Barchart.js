4e766987a37cbc12daa3265b2daadee9d50c3098
Kaushik Mishra <kmishra27.km@gmail.com>
2020-10-26 18:39:54 +0530

    comments added

--- src/components/common/barchart/Barchart.js
+++ src/components/common/barchart/Barchart.js
@@ -8,33 +8,34 @@ import { HorizontalBar } from 'react-chartjs-2';
 function Barchart(props) {
 
     const chartData = {
-        // 
-        labels : props.labels,
+        labels : props.labels, // list of strings for labels
         datasets : [
             {   
-                label: props.dataLabel,
-                backgroundColor : props.colour,
-                data: props.record
+                label: props.dataLabel, // name of the legend
+                backgroundColor : props.colour, // colour of the bar 
+                data: props.record // list of integer values as data
             }
         ],
     }
 
     return (
-        <div className="container">
+        <>
+            {/* As the background of the graphs provided by chart js is transparent we can't directly apply any colour to it,   
+            So I wrapped it around a div where we can apply the colour*/}
             <div className="bar" style={{backgroundColor: props.backgroundColour}}>
                 <HorizontalBar
                     data={chartData}
                     options={{
                         title: {
-                            display: true,
-                            text: props.title,
+                            display: true, 
+                            text: props.title, // title of the graph
                             fontSize: 18,
                             fontColor: 'black',
                             fontStyle: 'bold',
                             fontFamily: 'Arial'
                         },
                         legend: {
-                            display: true,
+                            display: true, // if false the legend will be hidden
                             position: 'bottom',
                             align: 'center',
                             labels : {
@@ -44,7 +45,7 @@ function Barchart(props) {
                         scales: {
                             yAxes: [{
                                 gridLines: {
-                                    display: false
+                                    display: false // it true the y axes scales will be shown
                                 },
                                 ticks: {
                                     fontColor: "black",
@@ -53,7 +54,7 @@ function Barchart(props) {
                             }],
                             xAxes: [{
                                 gridLines: {
-                                    display: true
+                                    display: true // it false the x axes scales will not be shown
                                 },
                                 ticks: {
                                     fontColor: "black",
@@ -64,7 +65,7 @@ function Barchart(props) {
                     }}
                 />
             </div>
-        </div>
+        </>
     )
 }
 
