f4ff0e5bc28966aafb1a0d72c90d7605e66b52cd
Denis <48964863+Denisqo@users.noreply.github.com>
2020-11-07 14:02:08 +0100

    update DB class

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,8 @@ class DB {
     schema;
 
     constructor(key, schema) {
-        if (typeof key !== 'string' || typeof schema !== 'object' || key.length === 0 || schema.length === 0) {
-            return new Error('Bad parameter "key" or "schema"')
+        if (typeof key !== 'string' || !Array.isArray(schema) || key.length === 0 || schema.length === 0) {
+            throw new Error('Bad parameter "key" or "schema"')
         }
         this.key = key
         this.schema = schema
@@ -12,11 +12,11 @@ class DB {
 
     insert(entity) {
         if (typeof entity !== 'object' || Object.keys(entity).length === 0 || Object.keys(entity).length !== this.schema.length) {
-            return new Error('Bad parameter "entity"')
+            throw new Error('Bad parameter "entity"')
         }
 
-        if (!this.isSameKeys()) {
-            return new Error('The entity object is not of the same type as the schema')
+        if (!this.isSameKeys(entity)) {
+            throw new Error('The entity object is not of the same type as the schema')
         }
 
         const localStorageItem = window.localStorage.getItem(this.key)
